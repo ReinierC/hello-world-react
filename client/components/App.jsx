c26ed5e606157cc6f310f5fb0f2ed6205760ae2c
reinier <reinier.caron@protonmail.com>
2018-01-16 14:58:09 +0100

    Working react environment | Reinier Caron

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello World from App.jsx</h1>
+        <p>Made using a walkthrough found on: 
+        <a target="_blank" href="https://scotch.io/tutorials/setup-a-react-environment-using-webpack-and-babel">scotch.io</a></p>
+      </div>);
+  }
+}
